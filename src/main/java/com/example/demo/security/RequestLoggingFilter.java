148ef81da05ff7822ed59601bef852ab58f9d47f
donghung1999 <donghung18061999@gmail.com>
2026-09-10 08:37:44 +0700

    Add logging color

--- src/main/java/com/example/demo/security/RequestLoggingFilter.java
+++ src/main/java/com/example/demo/security/RequestLoggingFilter.java
@@ -13,6 +13,12 @@ import java.io.IOException;
 @Component
 @Slf4j
 public class RequestLoggingFilter extends OncePerRequestFilter {
+    private static final String RESET = "\u001B[0m";
+    private static final String GREEN = "\u001B[32m";
+    private static final String YELLOW = "\u001B[33m";
+    private static final String RED = "\u001B[31m";
+    private static final String CYAN = "\u001B[36m";
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         long start = System.currentTimeMillis();
@@ -20,12 +26,26 @@ public class RequestLoggingFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
         } finally {
             long duration = System.currentTimeMillis() - start;
+            int status = response.getStatus();
+            String statusColor;
+
+            if (status >= 200 && status < 300) {
+                statusColor = GREEN;
+            } else if (status >= 400 && status < 500) {
+                statusColor = YELLOW;
+            } else {
+                statusColor = RED;
+            }
+
             log.info(
-                    "API={} {} | IP={} | Status={} | Duration={}ms",
+                    CYAN + "{} {}" + RESET
+                            + " | Status=" + statusColor + "{}" + RESET
+                            + " | IP={}"
+                            + " | Duration={}ms",
                     request.getMethod(),
                     request.getRequestURI(),
+                    status,
                     request.getRemoteAddr(),
-                    response.getStatus(),
                     duration
             );
         }
